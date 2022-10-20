5fba9d205f2d98dec73777e8c22116622c8d831e
anoop-kmr <111988378+anoop-kmr@users.noreply.github.com>
2022-10-20 13:38:45 +0530

    Update ltd.py

--- ltd.py
+++ ltd.py
@@ -83,10 +83,10 @@ def extractDetails(pno):
                   if (studentDict["asin"] not in lowest_price) or int(lowest_price[studentDict["asin"]])>price:
                     lowest_price[studentDict["asin"]]=price
                     msg="\nLowest Price !!"
-                    req=requests.get('https://api.telegram.org/bot'+bot_token+'/sendMessage?chat_id='+group_id+'&text=https://www.amazon.in/dp/'+studentDict["asin"]+'/ref=ox_sc_saved_title_7?smid=A1X54IAKXCWO8D\n'+str(price)+'\n'+pct+'% off'+msg)
+                    req=requests.get('https://api.telegram.org/bot'+bot_token+'/sendMessage?chat_id='+group_id+'&text=https://www.amazon.in/dp/'+studentDict["asin"]+'/ref=ox_sc_saved_title_7?smid=A1X54IAKXCWO8D\n'+str(price)+'\n'+str(pct)+'% off'+msg)
                   elif int(lowest_price[studentDict["asin"]])<price:
                     msg="\nLowest Price: "+str(lowest_price[studentDict["asin"]])
-                    req=requests.get('https://api.telegram.org/bot'+bot_token+'/sendMessage?chat_id='+group_id+'&text=https://www.amazon.in/dp/'+studentDict["asin"]+'/ref=ox_sc_saved_title_7?smid=A1X54IAKXCWO8D\n'+str(price)+msg)
+                    req=requests.get('https://api.telegram.org/bot'+bot_token+'/sendMessage?chat_id='+group_id+'&text=https://www.amazon.in/dp/'+studentDict["asin"]+'/ref=ox_sc_saved_title_7?smid=A1X54IAKXCWO8D\n'+str(price)+'\n'+str(pct)+'% off'+msg)
                   #print(pdt)
                   print(req)
                   print(len(List),len(lowest_price))
