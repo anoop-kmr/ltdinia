b3bce34a8a2ac9a68b448b639282f866a2209730
anoop-kmr <111988378+anoop-kmr@users.noreply.github.com>
2023-04-09 14:58:14 +0530

    Update upd_price.sh

--- upd_price.sh
+++ upd_price.sh
@@ -3,7 +3,7 @@
 home_dir=$pwd
 
 # update the file
-curl -i -X PUT -H 'Authorization: token $1' -d "{\"path\": \"lowest.txt\", \
+curl -i -X PUT -H 'Authorization: token '$1 -d "{\"path\": \"lowest.txt\", \
 \"message\": \"update\", \"content\": \"$(openssl base64 -A -in ./lowest.txt)\", \"branch\": \"feature/updated_prices\",\
 \"sha\": $(curl -X GET https://api.github.com/repos/anoop-kmr/ltdinia/contents/lowest.txt?ref=feature/updated_prices | jq .sha)}" \
 https://api.github.com/repos/anoop-kmr/ltdinia/contents/lowest.txt?ref=feature/updated_prices
