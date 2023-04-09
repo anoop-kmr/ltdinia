f57f416663e5826d469046b956afa94accef4ff9
anoop-kmr <111988378+anoop-kmr@users.noreply.github.com>
2023-04-09 14:43:34 +0530

    Add files via upload

---
+++ upd_price.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+home_dir=$pwd
+
+# update the file
+curl -i -X PUT -H 'Authorization: token $1' -d "{\"path\": \"lowest.txt\", \
+\"message\": \"update\", \"content\": \"$(openssl base64 -A -in ./lowest.txt)\", \"branch\": \"feature/updated_prices\",\
+\"sha\": $(curl -X GET https://api.github.com/repos/anoop-kmr/ltdinia/contents/lowest.txt?ref=feature/updated_prices | jq .sha)}" \
+https://api.github.com/repos/anoop-kmr/ltdinia/contents/lowest.txt?ref=feature/updated_prices
