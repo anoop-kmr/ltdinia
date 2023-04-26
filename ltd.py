fadd7e33c5af4f444a645dad157216b56a154c9c
anoop-kmr <111988378+anoop-kmr@users.noreply.github.com>
2023-04-26 12:13:50 +0530

    Update ltd.py

--- ltd.py
+++ ltd.py
@@ -168,7 +168,7 @@ print('Waiting for the thread...')
 def self_ping():
   while True:
     time.sleep(540)
-    print(requests.head("https://ltdinia.onrender.com/"))
+    print(requests.get("https://ltdinia.onrender.com/"))
 thread3 = threading.Thread(None, self_ping)
 thread3.start()
 
