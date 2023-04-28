064a236e519172282013a482c5347cf1f11f8668
anoop-kmr <111988378+anoop-kmr@users.noreply.github.com>
2023-04-28 15:34:03 +0530

    Update ltd.py

--- ltd.py
+++ ltd.py
@@ -153,7 +153,7 @@ def extr():
   global i,pgno,git_token
   print("Thread Running!!")
   while i in range(1,pgno+1):
-    pgno=extractDetails(i)
+    pgno=threading.Thread(target=extractDetails,args=(i)).start()
     requests.get('https://api.telegram.org/bot'+bot_token+'/sendMessage?chat_id='+group_id+'&text='+str(i))
     i=i+1
     if i==pgno:
