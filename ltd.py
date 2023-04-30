9c43a0092a82293254cb5d5c324255bc482fbd5a
anoop-kmr <111988378+anoop-kmr@users.noreply.github.com>
2023-04-30 21:35:55 +0530

    Update ltd.py

--- ltd.py
+++ ltd.py
@@ -156,26 +156,22 @@ def extr():
     pgno=extractDetails(i)
     requests.get('https://api.telegram.org/bot'+bot_token+'/sendMessage?chat_id=ltdinia&text=Pgno: '+str(i)+' out of '+str(pgno))
     i=i+1
-#     if i==pgno:
-#       i=1
-#       time.sleep(10)
+    if i==pgno:
+      i=1
+      time.sleep(10)
 #       print(subprocess.run(["./upd_price.sh",git_token]))
+thr = threading.Thread(None, extr)
+thr.start()
 print('Waiting for the thread...')
 
 def self_ping():
   while True:
-    time.sleep(200)
+    time.sleep(640)
     filename="lowest.txt"
     repo = "anoop-kmr/ltdinia"
     branch="feature/updated_prices"
     push_to_github(filename, repo, branch, git_token)
     print(requests.get("https://ltdinia.onrender.com/"))
-    try:
-      threading.Thread(target=extr).start()
-#       thread1 = threading.Thread(None, extr)
-#       thread1.start()
-    except:
-      print('Unable to start thread')
 thread3 = threading.Thread(None, self_ping)
 thread3.start()
 
