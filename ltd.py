1257d9a13b2755eefee34ecb83d01208249af0c2
anoop-kmr <111988378+anoop-kmr@users.noreply.github.com>
2024-11-10 21:14:30 +0530

    Update ltd.py

--- ltd.py
+++ ltd.py
@@ -119,7 +119,7 @@ def extractDetails(pno):
   print(pno)
   #url = "https://www.amazon.in/s/query?i=merchant-items&me=A1X54IAKXCWO8D&page="+str(pno)+"&marketplaceID=A21TJRUUN4KGV"
   #url = "https://www.amazon.in/s/query?page="+str(pno)+"&rh=n%3A976419031%2Cp_n_condition-type%3A13736826031%2Cp_6%3AA1X54IAKXCWO8D"
-  url = "https://www.amazon.in/s/query?page="+str(pno)+"&rh=n:976419031,p_n_condition-type:13736826031"
+  url = "https://www.amazon.in/s?page="+str(pno)+"&rh=n:976419031,p_n_condition-type:13736826031"
 
   payload = json.dumps({
     "customer-action": "pagination"
